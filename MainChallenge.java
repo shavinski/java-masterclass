47a4e4de683cf8529fb31fb5b793fe5fcf004eaf
Jakob <shavinski.jakob@gmail.com>
2024-01-17 18:11:36 -0800

    Section 5 take break

---
+++ MainChallenge.java
@@ -0,0 +1,34 @@
+public class MainChallenge {
+    public static void main(String[] args) {
+        
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
